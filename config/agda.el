0b8d69fae0dc042876b71164e677588843be8ae7
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-09-28 14:20:54 +0200

    update agda keybindings

--- config/agda.el
+++ config/agda.el
@@ -9,17 +9,8 @@
 (load-file (let ((coding-system-for-read 'utf-8))
              (concat (file-name-directory agda-mode-location) "agda-input.el")))
 
-(add-hook 'agda2-mode-hook
-          (lambda ()
-            (local-set-key (kbd "M-n") 'agda2-next-goal)
-            (local-set-key (kbd "M-p") 'agda2-previous-goal)
-            (local-set-key (kbd "C-c C-g") 'agda2-give)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(agda2-include-dirs (quote ("/Users/adam/local/agda-stdlib/src" "."))))
+(define-key agda2-mode-map (kbd "M-n") 'agda2-next-goal)
+(define-key agda2-mode-map (kbd "M-p") 'agda2-previous-goal)
+(define-key agda2-mode-map [s-mouse-1] 'agda2-goto-definition-mouse)
 
 (provide 'agda)
