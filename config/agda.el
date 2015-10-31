6efdfa0588c981bbc3a939588a4b9159016bbf0a
Adam Sandberg Eriksson <adam@sandbergericsson.se>
2015-10-31 12:12:07 +0100

    agda mode

--- config/agda.el
+++ config/agda.el
@@ -1,9 +1,7 @@
 ;; Agda
 
-(defvar agda-mode-location (shell-command-to-string "agda-mode locate"))
-
 (load-file (let ((coding-system-for-read 'utf-8))
-             agda-mode-location))
+             (shell-command-to-string "agda-mode locate")))
 
 (require 'agda2-mode)
 (require 'agda-input)
@@ -13,6 +11,5 @@
 (define-key agda2-mode-map [s-mouse-1] 'agda2-goto-definition-mouse)
 (define-key agda2-mode-map (kbd "s-]") 'agda2-goto-definition-keyboard)
 (define-key agda2-mode-map (kbd "s-[") 'agda2-go-back)
-
 
 (provide 'agda)
